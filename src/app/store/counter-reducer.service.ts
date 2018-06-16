bea3024f58cd2efe46b77841b94c0769deeb1344
Marco <marcodiplacido@gmail.com>
2018-06-15 23:49:41 -0400

    adding comment

--- src/app/store/counter-reducer.service.ts
+++ src/app/store/counter-reducer.service.ts
@@ -25,6 +25,7 @@ export class CounterReducerService {
   }
 
   constructor() {
+    // https://github.com/zalmoxisus/redux-devtools-extension
     this.counterStore = createStore(counter,
       (window as any).__REDUX_DEVTOOLS_EXTENSION__ &&
       (window as any).__REDUX_DEVTOOLS_EXTENSION__());
